d03c839df38d0c34a1959e74276dfbb4eccc249a
olivia leese <116760201+ol287@users.noreply.github.com>
2024-02-02 09:44:37 +0000

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void printBytes(void *ptr, int numBytes) {
+    // We use a char pointer to be able to access each byte of the memory.
+    unsigned char *p = (unsigned char *)ptr;
+
+    printf("Starting at memory address %p:\n", ptr);
+    for (int i = 0; i < numBytes; i++) {
+        printf("%03d: %d\n", i + 1, p[i]);
+    }
+}
+
+int main() {
+    // Example usage of printBytes
+    unsigned char someData[] = {10, 20, 30, 145};
+    printBytes(someData, 4);
+
+    return 0;
+}
