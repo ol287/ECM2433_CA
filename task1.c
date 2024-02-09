4d822dc6486685d06c030d194306388cd5ca3a4c
olivia leese <116760201+ol287@users.noreply.github.com>
2024-02-09 11:17:03 +0000

    Update task1.c
    calling the Printbyte funciton with a couple of examples

--- task1.c
+++ task1.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+void printBytes(void *ptr, int numBytes);
+
+int main() {
+    int num = 0x12345678;
+    float fnum = 123.456f;
+    char str[] = "Hello, World!";
+
+    // Test with an integer
+    printf("Bytes of an integer:\n");
+    printBytes(&num, sizeof(num));
+
+    // Test with a float
+    printf("\nBytes of a float:\n");
+    printBytes(&fnum, sizeof(fnum));
+
+    // Test with a string
+    printf("\nBytes of a string:\n");
+    printBytes(str, sizeof(str) - 1); // -1 to not include the null terminator
+
+    return 0;
+}
+
 void printBytes(void *ptr, int numBytes) {
     // We use a char pointer to be able to access each byte of the memory.
     unsigned char *p = (unsigned char *)ptr;
@@ -9,13 +31,5 @@ void printBytes(void *ptr, int numBytes) {
         printf("%03d: %d\n", i + 1, p[i]);
     }
 }
-
-// Example usage of printBytes
-//int main() {
-    //unsigned char someData[] = {10, 20, 30, 145};
-    //printBytes(someData, 4);
-
-    //return 0;
-//}
 
 
