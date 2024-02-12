07396afe8a5af5cda0b8d6d8c0f7a1909521b7db
olivia leese <116760201+ol287@users.noreply.github.com>
2024-02-12 13:15:35 +0000

    Update task1.c
    Commenting code

--- task1.c
+++ task1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//function prototype
 void printBytes(void *ptr, int numBytes);
 
 int main() {
@@ -7,29 +8,32 @@ int main() {
     float fnum = 123.456f;
     char str[] = "Hello, World!";
 
-    // Test with an integer
+    //test with an integer
     printf("Bytes of an integer:\n");
     printBytes(&num, sizeof(num));
 
-    // Test with a float
+    //test with a float
     printf("\nBytes of a float:\n");
     printBytes(&fnum, sizeof(fnum));
 
-    // Test with a string
+    //test with a string
     printf("\nBytes of a string:\n");
     printBytes(str, sizeof(str) - 1); // -1 to not include the null terminator
 
     return 0;
 }
 
+//function that takes a void pointer and interger as parameters
 void printBytes(void *ptr, int numBytes) {
-    // We use a char pointer to be able to access each byte of the memory.
+    //use char pointer to be able to access each byte of the memory
     unsigned char *p = (unsigned char *)ptr;
-
     printf("Starting at memory address %p:\n", ptr);
+    //loop through each byte of mmeory
     for (int i = 0; i < numBytes; i++) {
         printf("%03d: %d\n", i + 1, p[i]);
     }
 }
+
+
 
 
