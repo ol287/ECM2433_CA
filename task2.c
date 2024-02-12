b3524cfb54b071e48fc21c9cf9ad2f606c0efe24
olivia leese <116760201+ol287@users.noreply.github.com>
2024-02-12 21:13:06 +0000

    Update task2.c
    Fully commented code

--- task2.c
+++ task2.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Function to reverse the contents of a file
 int reverseFileContents(const char *inputFileName, const char *outputFileName) {
-    // Open the input file for reading
+    // Try to open the input file
     FILE *fileIn = fopen(inputFileName, "r");
     if (fileIn == NULL) {
         perror("Error opening input file");
-        return EXIT_FAILURE;
+        return EXIT_FAILURE; 
     }
 
-    // Open the output file for writing
+    // Try to open the input file
     FILE *fileOut = fopen(outputFileName, "w");
     if (fileOut == NULL) {
         perror("Error opening output file");
-        fclose(fileIn); // Close the input file before exiting
-        return EXIT_FAILURE;
+        // close input file
+        fclose(fileIn); 
+        return EXIT_FAILURE; 
     }
 
-    // Go to the end of the fileIn
+    // Move to the end of the input file so we can start reading from the back.
     if (fseek(fileIn, 0, SEEK_END) != 0) {
         perror("Error seeking in input file");
         fclose(fileIn);
@@ -25,7 +27,7 @@ int reverseFileContents(const char *inputFileName, const char *outputFileName) {
         return EXIT_FAILURE;
     }
 
-    // Get the position of the last character (size of the file)
+    // Find out how big the file is, which tells us where the last character is.
     long fileSize = ftell(fileIn);
     if (fileSize == -1) {
         perror("Error telling position in input file");
@@ -34,29 +36,28 @@ int reverseFileContents(const char *inputFileName, const char *outputFileName) {
         return EXIT_FAILURE;
     }
 
-    // Read from the end of fileIn and write to fileOut in reverse
+    // read each character in the file starting from the last one
     for (long pos = fileSize - 1; pos >= 0; --pos) {
-        // Seek to the next character from the end
         if (fseek(fileIn, pos, SEEK_SET) != 0) {
             perror("Error seeking in input file");
             break;
         }
 
-        // Read a character
+        // Read a single character from the file.
         int ch = fgetc(fileIn);
-        if (ch == EOF) {
+        if (ch == EOF) { 
             perror("Error reading character");
             break;
         }
 
-        // Write the character
+        // Write that character into the output file.
         if (fputc(ch, fileOut) == EOF) {
             perror("Error writing character");
             break;
         }
     }
 
-    // Close the files
+    // close both files
     fclose(fileIn);
     fclose(fileOut);
 
@@ -64,12 +65,10 @@ int reverseFileContents(const char *inputFileName, const char *outputFileName) {
 }
 
 int main(int argc, char *argv[]) {
-    // Check if the number of arguments is correct
+    // Check if the user ran the program with the right number of arguments: the program name, input file name, and output file name.
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s fileIn fileOut\n", argv[0]);
-        return EXIT_FAILURE;
+        fprintf(stderr, "Usage: %s fileIn fileOut\n", argv[0]); 
+        return EXIT_FAILURE; 
     }
-
-    // Call the function to reverse the contents of the file
     return reverseFileContents(argv[1], argv[2]);
 }
