deb91f4d5c73b981f2c247ef4f8226dad35d43f9
olivia leese <116760201+ol287@users.noreply.github.com>
2024-02-18 16:04:01 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Define the source files
-SRC_FILES="task1.c task2.c task3.c" # Specify your C source files here
+# Compile task1.c, task2.c, and task3.c with ANSI C standard compliance
+gcc -ansi -c task1.c -o task1.o
+gcc -ansi -c task2.c -o task2.o
+gcc -ansi -c task3.c -o task3.o
 
-# Include directory for the header file if needed
-# For example, if msString.h is in the current directory, no need to specify
-# If it's in a 'include' directory, you can add: -I./include
+# Link the object files into a single executable named coursework
+gcc task1.o task2.o task3.o -o coursework
 
-# Compile and link the program
-gcc -std=c99 -o coursework $SRC_FILES -I.
-
-# Check if the compilation was successful
+# Check if the compilation and linking were successful
 if [ $? -eq 0 ]; then
-    echo "Compilation successful."
+    echo "Compilation and linking successful. Executable named 'coursework' created."
 else
-    echo "Compilation failed."
-    exit 1
+    echo "An error occurred during compilation or linking."
 fi
